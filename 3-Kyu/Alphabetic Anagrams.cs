cef4b3bcb6d8782e4d0e2bac81179036826a5b29
LBiglieri <lautarobiglieri@hotmail.com>
2021-07-12 09:03:34 -0300

    Desafío diario: Alphabetic Anagrams. 12-07-2021

---
+++ 3-Kyu/Alphabetic Anagrams.cs
@@ -0,0 +1,89 @@
+//Consider a "word" as any sequence of capital letters A-Z (not limited to just "dictionary words"). For any word with at least two different letters, there are other words composed of the same letters but in a different order (for instance, STATIONARILY/ANTIROYALIST, which happen to both be dictionary words; for our purposes "AAIILNORSTTY" is also a "word" composed of the same letters as these two).
+
+//We can then assign a number to every word, based on where it falls in an alphabetically sorted list of all words made up of the same group of letters. One way to do this would be to generate the entire list of words and find the desired one, but this would be slow if the word is long.
+
+//Given a word, return its number. Your function should be able to accept any word 25 letters or less in length (possibly with some letters repeated), and take no more than 500 milliseconds to run. To compare, when the solution code runs the 27 test cases in JS, it takes 101ms.
+
+//For very large words, you'll run into number precision issues in JS (if the word's position is greater than 2^53). For the JS tests with large positions, there's some leeway (.000000001%). If you feel like you're getting it right for the smaller ranks, and only failing by rounding on the larger, submit a couple more times and see if it takes.
+
+//Python, Java and Haskell have arbitrary integer precision, so you must be precise in those languages (unless someone corrects me).
+
+//C# is using a long, which may not have the best precision, but the tests are locked so we can't change it.
+
+//Sample words, with their rank:
+//ABAB = 2
+//AAAB = 1
+//BAAA = 4
+//QUESTION = 24572
+//BOOKKEEPER = 10743
+
+using System.Collections.Generic;
+using System.Linq;
+public class Kata
+{
+    private static long GetSubPermutations(Dictionary<char, int> frequencies, char currentLetter)
+    {
+        frequencies[currentLetter]--;
+        long denominator = 1;
+        foreach (var key in frequencies)
+        {
+            var subPermutations = Factorial(frequencies[key.Key]);
+            denominator *= subPermutations != 0 ? subPermutations : 1;
+        }
+        frequencies[currentLetter]++;
+        return denominator;
+    }
+
+    private static long Factorial(int n)
+    {
+        int temp = n;
+        long output = n;
+        while (temp-- > 1)
+        {
+            output *= temp;
+        }
+        return output;
+    }
+    public static long ListPosition(string value)
+    {
+
+        List<char> sortedLetters = value.ToCharArray().ToList();
+        sortedLetters.Sort();
+        Dictionary<char, int> frequencies = new Dictionary<char, int>();
+        long index = 1;
+        int remainingLetters = value.Length - 1;
+        int splitStringIndex = 0;
+        foreach (char letter in sortedLetters.Distinct())
+        {
+            frequencies.Add(letter, sortedLetters.Count(x => x == letter));
+        }
+
+        while (sortedLetters.Count > 0)
+        {
+            for (var i = 0; i < sortedLetters.Count; i++)
+            {
+                if (sortedLetters[i] != value[splitStringIndex])
+                {
+                    if (sortedLetters[i] != sortedLetters[i + 1])
+                    {
+                        var permutations = Factorial(remainingLetters);
+                        index += permutations / GetSubPermutations(frequencies, sortedLetters[i]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    splitStringIndex++;
+                    frequencies[sortedLetters[i]]--;
+                    sortedLetters.RemoveRange(i, 1);
+                    remainingLetters--;
+                    break;
+                }
+            }
+        }
+        return index;
+    }
+}
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ _Repositorio para subir desafios diarios resueltos de la plataforma CodeWars._
 Los desafíos se encuentran organizados por dificultad que es medida en Kyu, cuanto más bajo es el número, más alta la dificultad. Hay un archivo .cs con el nombre de cada desafío y dentro de los mismos se encuentra comentado el enunciado del mismo.(no se incluyen los tests unitarios que realiza la plataforma sobre el código suscripto).
 
 ## Cantidad de desafíos realizados 💪
-Actualmente hay 27 desafíos realizados en C#.
+Actualmente hay 28 desafíos realizados en C#.
 
 ## Profile Badge 🛡
 Mi Profile Badge de CodeWars : ![alt text](https://www.codewars.com/users/LBiglieri/badges/small)
